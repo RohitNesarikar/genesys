c91edcc76e0a86310224519a655251715da4eca5
RohitNesarikar <57177344+RohitNesarikar@users.noreply.github.com>
2019-10-30 16:20:18 +0530

    Add files via upload

---
+++ sort.c
@@ -0,0 +1,38 @@
+/*
+Name:Rohit Nesarikar
+Purpose :KPIT Virtual Genesys Assignment for 1.A3
+Problem:Find the biggest four digit number, by deleting a single digit from 5 digit number
+*/
+#include<stdio.h>
+int main()
+{
+    int arr[5];     //array to store digits
+    int n,tempn;    //store number and creating a backup
+    int i,j;        //looping variables
+    printf("\n Enter 5 digit number \n"); //reading 5 digit number
+    scanf("%d",&n);
+    tempn=n;
+    for(i=0;i<5;i++)    //reading digits
+    {
+        arr[i]=tempn%10;
+        tempn=tempn/10;
+    }
+    for(i=0;i<5;i++)    //sort digits
+    {
+        for(j=i+1;j<5;j++)
+        {
+            if(arr[i]<arr[j])
+            {
+                tempn=arr[i];
+                arr[i]=arr[j];
+                arr[j]=tempn;
+            }
+        }
+    }
+    printf("\nThe max 4 digit number that can be formed is:\t");    //print highest 4 digit
+    for(i=0;i<4;i++)
+    {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
